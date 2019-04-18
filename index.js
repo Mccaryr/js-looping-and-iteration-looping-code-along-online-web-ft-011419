421cb6cc975c15e368a50ef3d606bed013706994
Robert McCary <robert.mccary@yahoo.com>
2019-04-18 01:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const employeeNames = ['Ada', 'Brendan', 'Ali']
-function printBadges(employeeNames) {
-  for (let i=0; i < employeeNames.length; i++){
-    console.log("Welcome ${employeeNames[i]}! You are employee #${i + 1}.")
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
-  return employeeNames
+
+  return names;
 }
 
-function tailsNeverFails() {
+function tailsNeverFails () {
   let tails = 0;
-  while (Math.random() >= 0.5){
+
+  while (Math.random() >= 0.5) {
     tails++;
   }
-  return "You got ${heads} tails in a row!";
+
+  return `You got ${tails} tails in a row!`;
 }
