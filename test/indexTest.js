4c5ac9712725081b2a85698a594170511e509d48
Robert McCary <robert.mccary@yahoo.com>
2019-04-18 01:43:47 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,42 +1,42 @@
-const sinon = require( 'sinon' )
-
-describe( 'index.js', () => {
-  describe( 'printBadges()', () => {
-    let spy;
-
-    beforeEach( () => {
-      spy = sinon.spy( console, 'log' );
-    } );
-
-    afterEach( () => {
-      spy.restore();
-    } );
-
-    it( 'invokes console.log() once for each element in the passed-in array', () => {
-      printBadges( [ 'Lisa', 'Kaitlin', 'Jan' ] );
-
-      expect( spy.callCount )
-        .to.eq( 3 );
-    } );
-
-    it( 'prints out a welcome badge for each employee', () => {
-      printBadges( [ 'Joe', 'Gabe' ] );
-
-      expect( spy.calledWithExactly( 'Welcome Joe! You are employee #1.' ) )
-        .to.be
-        .true;
-      expect( spy.calledWithExactly( 'Welcome Gabe! You are employee #2.' ) )
-        .to.be
-        .true;
-    } );
-
-    it( 'returns the passed-in array', () => {
-      const arr = [];
-
-      expect( printBadges( arr ) )
-        .to.eq( arr );
-    } );
-  } );
+// const sinon = require( 'sinon' )
+//
+// describe( 'index.js', () => {
+//   describe( 'printBadges()', () => {
+//     let spy;
+//
+//     beforeEach( () => {
+//       spy = sinon.spy( console, 'log' );
+//     } );
+//
+//     afterEach( () => {
+//       spy.restore();
+//     } );
+//
+//     it( 'invokes console.log() once for each element in the passed-in array', () => {
+//       printBadges( [ 'Lisa', 'Kaitlin', 'Jan' ] );
+//
+//       expect( spy.callCount )
+//         .to.eq( 3 );
+//     } );
+//
+//     it( 'prints out a welcome badge for each employee', () => {
+//       printBadges( [ 'Joe', 'Gabe' ] );
+//
+//       expect( spy.calledWithExactly( 'Welcome Joe! You are employee #1.' ) )
+//         .to.be
+//         .true;
+//       expect( spy.calledWithExactly( 'Welcome Gabe! You are employee #2.' ) )
+//         .to.be
+//         .true;
+//     } );
+//
+//     it( 'returns the passed-in array', () => {
+//       const arr = [];
+//
+//       expect( printBadges( arr ) )
+//         .to.eq( arr );
+//     } );
+//   } );
 
 //   describe( 'tailsNeverFails()', () => {
 //     let spy;
