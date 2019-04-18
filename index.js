bdfacc55c60c2182cd544d45a6f9aee2c1e67551
Robert McCary <robert.mccary@yahoo.com>
2019-04-18 01:35:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const employeeNames = ['Ada', 'Brendan', 'Ali']
 function printBadges(employeeNames) {
-  for (let i=0; i < employeeNames.length, i++){
-    console.log("Welcome ${employeeNames}! You are employee #[i].")
+  for (let i=0; i < employeeNames.length; i++){
+    console.log("Welcome ${employeeNames[i]}! You are employee #${i + 1}.")
   }
   return employeeNames
 }
